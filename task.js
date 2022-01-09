c32923073ccbd36f5d1d9bcb8caa57b687ecc66f
de-bratchikov <de-bratchikov@yandex.ru>
2022-01-09 23:01:58 +0500

    Second commit

--- task.js
+++ task.js
@@ -1,8 +1,26 @@
+"use strict"
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  D = Math.pow(b, 2) - (4 * a * c);
+  if (D === 0) {
+    let x;
+    x = -b / (2 * a);
+    arr.push(x);
+  } else if (D > 0) {
+    let x1;
+    let x2;
+    x1 = (-b + Math.sqrt(D)) / (2 * a);
+    x2 = (-b - Math.sqrt(D)) / (2 * a);
+    arr.push(x1, x2);
+  } else if (D < 0) {
+    console.log("Дискриминант меньше нуля")
+  }
+  return arr;
 }
+
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
