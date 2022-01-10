4c8bdbb464020a7db79b6e4de8069d1ddadfd639
de-bratchikov <de-bratchikov@yandex.ru>
2022-01-10 14:50:59 +0500

    Third commit 1 task is ready

--- task.js
+++ task.js
@@ -2,24 +2,25 @@
 
 function solveEquation(a, b, c) {
   let arr = [];
-  D = Math.pow(b, 2) - (4 * a * c);
-  if (D === 0) {
+  let d;
+  d = Math.pow(b, 2) - (4 * a * c);
+  if (d === 0) {
     let x;
     x = -b / (2 * a);
     arr.push(x);
-  } else if (D > 0) {
+    console.log(arr);
+  } else if (d > 0) {
     let x1;
     let x2;
-    x1 = (-b + Math.sqrt(D)) / (2 * a);
-    x2 = (-b - Math.sqrt(D)) / (2 * a);
+    x1 = (-b + Math.sqrt(d)) / (2 * a);
+    x2 = (-b - Math.sqrt(d)) / (2 * a);
     arr.push(x1, x2);
-  } else if (D < 0) {
-    console.log("Дискриминант меньше нуля")
+    console.log(arr);
+  } else if (d < 0) {
+    return(arr);
   }
-  return arr;
+  return(arr);
 }
-
-
 
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
