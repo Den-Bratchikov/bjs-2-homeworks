3e148bab2e38e2177f0279da6ff44c8a1f91e59a
de-bratchikov <de-bratchikov@yandex.ru>
2022-01-21 17:20:20 +0500

    Theme 3 Tasks 1,2 ready (ed.1)

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
+  let result = (arr1.every === arr2.every) && (arr1.length === arr2.length);
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+  let positNumber = n => n > 0 && n % 3 ===0;
+  let arr1 = arr.filter(positNumber);
+  let resultArr = arr1.map(item => item * 10);
 
   return resultArr; // array
 }
