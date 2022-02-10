637c2f87b3eb7199fdbc55f05bb79e468a9960b3
de-bratchikov <de-bratchikov@yandex.ru>
2022-02-10 22:32:21 +0500

    Theme 4 Task 1 ready (ed.1)

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,40 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  return {
+    name,
+    gender,
+    age,
+  }
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  if(this.marks === undefined){ 
+    this.marks = marks.push (mark); 
+    } else {
+      this.marks = marks.push (mark);
+    }
+}
+
+Student.prototype.addMarks = function(mark1, mark2, mark3,...markX) {
+  for (let i = 0; i < marks.length; i++) {
+    this.marks = marks.push(markX[i]);
+}
+
+Student.prototype.getAverage = function() {
+  for (let i = 0; i < this.marks.length; i++) {
+    total += this.marks[i];
+  }
+  
+  return total / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  
+  delete this.marks;
+  
+  this.excluded = reason;
+}
